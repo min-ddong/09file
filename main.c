26959c0d0561414e915d9612e2e1fe560647c004
min-ddong <jellykim907@gmail.com>
2020-10-29 16:48:03 +0900

    fifth

--- main.c
+++ main.c
@@ -1,18 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define ROWS 3
+#define COLS 3
+
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-	int i;
-int grade[5];
+int main(void)
+{
+int A[ROWS][COLS] = {
+        {2,3,0},
+		{8,9,1},
+		{7,0,5} };
 
- grade[0] = 10;
- grade[1] = 20;
- grade[2] = 30;
- grade[3] = 40;
- grade[4] = 50;
+int B[ROWS][COLS] = {
+        {1,0,0},
+        {0,1,0},
+        {0,0,1} };
+        
+int C[ROWS][COLS];
 
-for (i=0; i<5; i++)
-	return 0;
+addMatrix(A,B,C);
+printMatrix(C);
+
+return 0; }
+        
+
+void addMatrix(int A[][COLS], int B[][COLS], int C[][COLS])
+{
+	int i, j;
+	for(i=0;i<ROWS;i++)
+	   for(j=0;j<COLS;j++)
+	      C[i][j] = A[i][j] + B[i][j];
+}
+
+void printMatrix(int A[][COLS])
+{
+	int i, j;
+	for(i=0; i<ROWS; i++)
+	{
+		for(j=0;j<COLS;j++)
+		   printf("%d", A[i][j]);
+		printf("\n");   
+	}
 }
